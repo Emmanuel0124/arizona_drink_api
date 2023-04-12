317e4289cf45026e5a92a7998a711efc03580ecb
EmmanuelBahena <mannybahena04@gmail.com>
2023-04-12 13:17:29 -0500

    add index action

--- test/controllers/flavors_controller_test.rb
+++ test/controllers/flavors_controller_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class FlavorsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index" do
+    get/ "flavors.json"
+    assert_response 200
+
+    data = json.parse(response.body)
+    assert_equal Flavor.count, data.length
+  end
 end
